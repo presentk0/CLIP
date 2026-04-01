ffa40ef0f244749ff43e0eac69e80bf83358dade
presentk <632366@naver.com>
2026-04-01 23:01:50 +0900

    [EDSD-9] feat: 프로젝트 초기 세팅 및 .gitignore 보안 설정

---
+++ backend/clip-server/src/main/java/com/clip/server/ClipServerApplication.java
@@ -0,0 +1,13 @@
+package com.clip.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipServerApplication.class, args);
+	}
+
+}
---
+++ backend/clip-server/src/test/java/com/clip/server/ClipServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clip.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
