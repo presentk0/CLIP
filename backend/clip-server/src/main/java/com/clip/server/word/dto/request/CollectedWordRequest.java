f27ff29f79ed62eefaecec9aa2ef342c3ea56231
presentk <hyounjae94@gmail.com>
2026-04-13 16:05:20 +0900

    [EDSD-13] feat: 단어수집 기능 구현 및 테스트 코드 작성

--- backend/clip-server/src/main/java/com/clip/server/word/dto/request/CollectedWordRequest.java
+++ backend/clip-server/src/main/java/com/clip/server/word/dto/request/CollectedWordRequest.java
@@ -1,5 +1,7 @@
 package com.clip.server.word.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CollectedWordRequest {
 
-    // 수집하려는 단어의 영상 Id
-    private String videoId;
-    // 수집하려는 영상 제목
-    private String title;
-    // 수집하려는 단어
-    private String word;
-    // 수집하려는 단어가 포함된 문장
-    private String sentence;
-    // 수집하려는 단어의 영상 타임스탬프
-    private String timestamp;
-    // 수집하려는 단어가 포함된 문장 번역
-    private String translation;
+    @NotBlank(message = "영상 Id는 필수입니다.")
+    private String videoId; // 수집하려는 단어의 영상 Id
+    @NotBlank(message = "영상 제목은 필수입니다.")
+    private String title; // 수집하려는 영상 제목
+    @NotBlank(message = "단어는 필수입니다.")
+    private String word; // 수집하려는 단어
+    @NotBlank(message = "문장은 필수입니다.")
+    private String sentence; // 수집하려는 단어가 포함된 문장
+    @NotBlank(message = "영상 타임스탬프는 필수입니다.")
+    private String timestamp; // 수집하려는 단어의 영상 타임스탬프
+    @NotBlank(message = "문장 번역은 필수입니다.")
+    private String translation; // 수집하려는 단어가 포함된 문장 번역
 }
