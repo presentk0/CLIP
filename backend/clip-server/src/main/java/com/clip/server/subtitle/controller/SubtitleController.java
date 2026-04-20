9e5850259eb7b083dd4a77155d13bae3414bba18
presentk <hyounjae94@gmail.com>
2026-04-20 11:23:15 +0900

    [EDSD-48] feat: 영상 자막조회 기능 구현 및 테스트 코드 작성

--- backend/clip-server/src/main/java/com/clip/server/subtitle/controller/SubtitleController.java
+++ backend/clip-server/src/main/java/com/clip/server/subtitle/controller/SubtitleController.java
@@ -2,6 +2,7 @@ package com.clip.server.subtitle.controller;
 
 import com.clip.server.common.response.ApiResponse;
 import com.clip.server.subtitle.dto.request.SubtitleRequest;
+import com.clip.server.subtitle.dto.respnse.SubtitleListResponse;
 import com.clip.server.subtitle.dto.respnse.SubtitleResponse;
 import com.clip.server.subtitle.service.SubtitleService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,6 +33,14 @@ public class SubtitleController {
                 .body(ApiResponse.success(subtitleResponse,"자막이 성공적으로 저장되었습니다."));
     }
 
-
+    @Operation(summary = "해당 영상의 전체 자막 조회 API")
+    @GetMapping
+    ApiResponse<SubtitleListResponse> getVideoSubtitles(
+            @PathVariable String videoId
+    ) {
+        Long tempUserId = 1L;
+        SubtitleListResponse subtitleListResponse = subtitleService.getSubtitles(videoId, tempUserId);
+        return ApiResponse.success(subtitleListResponse, "해당 영상의 자막이 성공적으로 조회되었습니다.");
+    }
 
 }
