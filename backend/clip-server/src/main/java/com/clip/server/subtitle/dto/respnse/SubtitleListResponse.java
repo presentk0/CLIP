4a7b67f71cb7ee28da8a091f79084eaaae481ed7
presentk <hyounjae94@gmail.com>
2026-04-20 10:35:38 +0900

    [EDSD-49] feat: 영상 자막조회 Service 및 테스트 코드 작성

---
+++ backend/clip-server/src/main/java/com/clip/server/subtitle/dto/respnse/SubtitleListResponse.java
@@ -0,0 +1,18 @@
+package com.clip.server.subtitle.dto.respnse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SubtitleListResponse {
+
+    private String videoId;
+    private List<SubtitleDetailResponse> subtitles;
+}
--- backend/clip-server/src/main/java/com/clip/server/subtitle/service/SubtitleService.java
+++ backend/clip-server/src/main/java/com/clip/server/subtitle/service/SubtitleService.java
@@ -2,6 +2,8 @@ package com.clip.server.subtitle.service;
 
 import com.clip.server.common.exception.BusinessException;
 import com.clip.server.subtitle.dto.request.SubtitleRequest;
+import com.clip.server.subtitle.dto.respnse.SubtitleDetailResponse;
+import com.clip.server.subtitle.dto.respnse.SubtitleListResponse;
 import com.clip.server.subtitle.dto.respnse.SubtitleResponse;
 import com.clip.server.subtitle.entity.Subtitle;
 import com.clip.server.subtitle.repository.SubtitleRepository;
@@ -13,9 +15,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.clip.server.common.exception.ErrorCode.USER_NOT_FOUND;
+import static com.clip.server.common.exception.ErrorCode.VIDEO_NOT_FOUND;
 
 
 @Service
@@ -62,9 +66,40 @@ public class SubtitleService {
         return mapToSubtitleResponse(subtitle);
     }
 
+    public SubtitleListResponse getSubtitles(String videoId, Long userId) {
+
+        // 1. 유저 정보 확인
+        User user = userRepository.findById(userId).orElseThrow(()-> new BusinessException(USER_NOT_FOUND));
+
+        // 영상 정보 존재 여부 확인
+        if (!videoRepository.existsById(videoId)) {
+            throw new BusinessException(VIDEO_NOT_FOUND);
+        }
+
+       List<SubtitleDetailResponse> subtitles = subtitleRepository.findByVideo_VideoIdOrderByStartTimeAsc(videoId)
+               .stream()
+               .map(this::mapToSubtitleDetailResponse)
+               .toList();
+
+       return SubtitleListResponse.builder()
+               .videoId(videoId)
+               .subtitles(subtitles)
+               .build();
+    }
+
     public SubtitleResponse mapToSubtitleResponse(Subtitle subtitle) {
         return SubtitleResponse.builder()
                 .videoId(subtitle.getVideo().getVideoId())
+                .subtitleId(subtitle.getId())
+                .text(subtitle.getText())
+                .translation(subtitle.getTranslation())
+                .startTime(subtitle.getStartTime())
+                .endTime(subtitle.getEndTime())
+                .build();
+    }
+
+    public SubtitleDetailResponse mapToSubtitleDetailResponse(Subtitle subtitle) {
+        return SubtitleDetailResponse.builder()
                 .subtitleId(subtitle.getId())
                 .text(subtitle.getText())
                 .translation(subtitle.getTranslation())
