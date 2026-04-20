106a12b25de9350d5c5ba663ef2ef34040b0ef51
presentk <hyounjae94@gmail.com>
2026-04-20 14:42:29 +0900

    [EDSD-38] feat: 호버/수집 단어 분리 저장

--- backend/clip-server/src/main/java/com/clip/server/word/repository/CollectedWordRepository.java
+++ backend/clip-server/src/main/java/com/clip/server/word/repository/CollectedWordRepository.java
@@ -3,17 +3,20 @@ package com.clip.server.word.repository;
 import com.clip.server.user.entity.User;
 import com.clip.server.video.entity.Video;
 import com.clip.server.word.entity.CollectedWord;
+import com.clip.server.word.entity.WordType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CollectedWordRepository extends JpaRepository<CollectedWord, Long> {
     boolean existsByUserAndWord(User user, String word);
     int countByUser(User user);
     Page<CollectedWord> findAll(Pageable pageable);
-    Page findAllByUserIdAndVideo_VideoId(Long userId, String videoId, Pageable pageable);
-    Page findAllByUserId(Long userId, Pageable pageable);
+    Page<CollectedWord> findAllByUserIdAndVideo_VideoIdAndWordType(Long userId, String videoId, WordType type, Pageable pageable);
+    Page<CollectedWord> findAllByUserIdAndWordType(Long userId, WordType type, Pageable pageable);
+    Optional<CollectedWord> findByUserIdAndVideo_VideoIdAndWord(Long userId, String videoId, String word);
 }
