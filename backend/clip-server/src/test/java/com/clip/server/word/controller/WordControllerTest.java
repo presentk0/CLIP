cece3e9089ec5f52e47ee13b025a695f4ce8e717
presentk <hyounjae94@gmail.com>
2026-05-19 09:22:46 +0900

    feat: Redis 설정 추가

--- backend/clip-server/src/test/java/com/clip/server/word/controller/WordControllerTest.java
+++ backend/clip-server/src/test/java/com/clip/server/word/controller/WordControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WordController.class)
+@ActiveProfiles("test")
 public class WordControllerTest {
 
     @Autowired
