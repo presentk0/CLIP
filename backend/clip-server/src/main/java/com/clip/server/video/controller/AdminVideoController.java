ee89b6ea6ffe195efcef70d65570883beed58ea3
presentk <hyounjae94@gmail.com>
2026-06-01 17:21:48 +0900

    fix: 관리자 API 호출 주소 수정

--- backend/clip-server/src/main/java/com/clip/server/video/controller/AdminVideoController.java
+++ backend/clip-server/src/main/java/com/clip/server/video/controller/AdminVideoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/videos")
+@RequestMapping("api/admin/videos")
 @RequiredArgsConstructor
 public class AdminVideoController {
 
