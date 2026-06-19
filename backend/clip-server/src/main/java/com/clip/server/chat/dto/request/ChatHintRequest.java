882c8133bdf7c777206b21ee2aee46f9217043b8
presentk <hyounjae94@gmail.com>
2026-06-19 15:39:07 +0900

    feat: AI 채팅 힌트 제안 로직 추가

---
+++ backend/clip-server/src/main/java/com/clip/server/chat/dto/request/ChatHintRequest.java
@@ -0,0 +1,12 @@
+package com.clip.server.chat.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatHintRequest {
+
+    private Long chatRoomId;
+}
